b07d51d5b49047c739620273242371fe80d75960
Sakrid1s <bilinets.vitalik@gmail.com>
2024-05-26 19:09:16 +0300

    upd

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -7,7 +7,6 @@ export const addOneContact = async () => {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
     const addContact = createFakeContact();
-
     contacts.push(addContact);
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
     console.log(`New contact was added`);
