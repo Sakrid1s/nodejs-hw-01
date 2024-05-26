638c0fe6f3d0e7310f7411e1731fb938dd6443b9
Sakrid1s <bilinets.vitalik@gmail.com>
2024-05-26 19:04:12 +0300

    upd

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,19 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    const addContact = createFakeContact();
+
+    contacts.push(addContact);
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log(`New contact was added`);
+  } catch (error) {
+    console.error(`Something went wrong:`, error);
+  }
+};
 
 await addOneContact();
